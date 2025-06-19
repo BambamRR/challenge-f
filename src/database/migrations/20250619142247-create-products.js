c2de7ce6f44722125908817e007a54364c3d7d86
BambamRR <leonardo.richard@pm.pe.gov.br>
2025-06-19 16:00:33 -0300

    chore: created migration for products

---
+++ src/database/migrations/20250619142247-create-products.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("products", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.STRING(300),
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      stock: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        onUpdate: "CURRENT_TIMESTAMP",
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        onUpdate: "CURRENT_TIMESTAMP",
+      },
+    });
+
+    await queryInterface.addConstraint("products", {
+      fields: ["price"],
+      type: "check",
+      name: "products_price_minimum",
+      where: {
+        price: {
+          [Sequelize.Op.gte]: 0.01,
+        },
+      },
+    });
+
+    await queryInterface.addConstraint("products", {
+      fields: ["stock"],
+      type: "check",
+      name: "products_stock_range",
+      where: Sequelize.literal,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("products");
+  },
+};
