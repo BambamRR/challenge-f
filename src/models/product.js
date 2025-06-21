f7f391337d0500ced9164c07e277f59d501951be
BambamRR <leonardo.richard@pm.pe.gov.br>
2025-06-21 10:00:25 -0300

    chore: created models for coupon and product

---
+++ src/models/product.js
@@ -0,0 +1,26 @@
+module.exports = (db, DataTypes) => {
+  const Product = db.define(
+    "Product",
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING(300),
+      price: DataTypes.DECIMAL(10, 2),
+      stock: DataTypes.INTEGER,
+      created_at: DataTypes.DATE,
+      updated_at: DataTypes.DATE,
+      deleted_at: DataTypes.DATE,
+    },
+    {
+      tableName: "products",
+      timestamps: true,
+      paranoid: true,
+      underscored: true,
+    }
+  );
+  Product.associate = (models) => {
+    Product.hasMany(models.ProductCouponApplication, {
+      foreignKey: "product_id",
+    });
+  };
+  return Product;
+};
