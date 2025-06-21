f54825dd04faea272c263862e8694d3df36ca66a
BambamRR <leonardo.richard@pm.pe.gov.br>
2025-06-21 20:43:16 -0300

    chore: created GET /product/{id}

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,9 @@ const ProductController = require('../controllers/ProductController')
 
 const router = require('express').Router()
 
-router.post('/', ProductController.createProduct)
+router
+.post('/', ProductController.createProduct)
+.get('/:id', ProductController.findProductById)
+
 
 module.exports = router
