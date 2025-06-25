410d66ddb26774598fe260288e0e11eeb422e8a2
BambamRR <leonardo.richard@pm.pe.gov.br>
2025-06-24 21:37:48 -0300

    chore: added coupon to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 //imported routes to use on project
 const product = require('./product')
+const coupon = require('./coupon')
 
 //routes
 router.use('/products', product)
+router.use('/coupon', coupon)
 
 module.exports = router
